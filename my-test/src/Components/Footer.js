ad9ae4824a69e551c5292c90a3576795099f89c6
Seva <seva@Efim-Babich-MacBook-Pro.local>
2022-11-10 19:27:48 -0800

    update

--- my-test/src/Components/Footer.js
+++ my-test/src/Components/Footer.js
@@ -1,13 +1,14 @@
 import React, { Component } from "react";
+import './Footer.css';
 
 export default class Footer extends Component {
     render() {
         return (
-            <div>
+            <div className="footer">
                 <p>The operator of this website is NOT a lender or a broker, does not make offers for loans and does not charge you for any service or products.
                      This site allows you to submit your information to our network of lenders to determine if they maybe be able to offer you a personal loan ranging from $100 up to $1,500.
                       The actual amounts, terms, and APR a customer could qualify for will vary based on credit scores, individual lender requirements, and state laws. Not all lenders can provide up to $1,500.
-                       Services are not available in all states. Credit checks may be obtained by some lenders. By using our content and services, you agree to our <a>Terms of Website Use</a>, Privacy Policy, and Disclaimer.</p>
+                       Services are not available in all states. Credit checks may be obtained by some lenders. By using our content and services, you agree to our <a href="#">Terms of Website Use</a>, <a href="#">Privacy Policy</a>, and <a href="#">Disclaimer</a>.</p>
                 <p>© 2020 NoName.com. All rights reserved.</p>
             </div>
         )
--- my-test/src/Components/Header.js
+++ my-test/src/Components/Header.js
@@ -18,23 +18,25 @@ export default class Header extends Component {
                 <div className="notification"> Welcome Back, John! <img src={close}/></div>
                 <Navbar collapseOnSelect expand="md">
                     <Container>
-                        <Navbar.Brand href="/">
-                            <img
-                                src={logo}
-                                height="36"
-                                width="57"
-                                className="d-inline-block align-top"
-                                alt="Logo"
-                            />
-                        </Navbar.Brand>
-                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                            <Nav className="mr-auto nav-header">
-                                <Nav.Link className="nav-item" href="/how_it_works">How It Works</Nav.Link>
-                                <Nav.Link className="nav-item" href="/faqs">FAQs</Nav.Link>
-                                <Button className="btn-login" href="/">Log In</Button>
-                            </Nav>
-                        </Navbar.Collapse>
+                        <div className="header">
+                            <Navbar.Brand href="/">
+                                <img
+                                    src={logo}
+                                    height="36"
+                                    width="57"
+                                    className="d-inline-block align-top"
+                                    alt="Logo"
+                                />
+                            </Navbar.Brand>
+                            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                                <Nav className="mr-auto nav-header">
+                                    <Nav.Link className="nav-item" href="/how_it_works">How It Works</Nav.Link>
+                                    <Nav.Link className="nav-item" href="/faqs">FAQs</Nav.Link>
+                                    <Button className="btn-login" href="/">Log In</Button>
+                                </Nav>
+                            </Navbar.Collapse>
+                        </div>
                     </Container>
                 </Navbar>
 
