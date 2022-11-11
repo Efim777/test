42a01f68ca5065a11394f8b2f3566832c6cf5972
Seva <seva@Efim-Babich-MacBook-Pro.local>
2022-11-10 18:28:06 -0800

    update

--- my-test/src/Components/Header.js
+++ my-test/src/Components/Header.js
@@ -2,31 +2,40 @@ import React, { Component } from "react";
 import {
     Navbar,
     Container,
+    Nav,
+    Button,
 } from "react-bootstrap";
+import './Header.css';
 import logo from '../Img/logo.svg'
+import close from '../Img/close.svg'
 
 export default class Header extends Component {
     render() {
         return (
-            <Navbar collapseOnSelect expand="md">
-                <Container>
-                    <Navbar.Brand href="/">
-                        <img
-                            src={logo}
-                            height="36"
-                            width="57"
-                            className="d-inline-block align-top"
-                            alt="Logo"
-                        />
-                    </Navbar.Brand>
-                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                        <Nav>
-                            
-                        </Nav>
-                    </Navbar.Collapse>
-                </Container>
-            </Navbar>
+            <>
+                <div className="notification"> Welcome Back, John! <img src={close}/></div>
+                <Navbar collapseOnSelect expand="md">
+                    <Container>
+                        <Navbar.Brand href="/">
+                            <img
+                                src={logo}
+                                height="36"
+                                width="57"
+                                className="d-inline-block align-top"
+                                alt="Logo"
+                            />
+                        </Navbar.Brand>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                            <Nav className="mr-auto nav-header">
+                                <Nav.Link className="nav-item" href="/how_it_works">How It Works</Nav.Link>
+                                <Nav.Link className="nav-item" href="/faqs">FAQs</Nav.Link>
+                                <Button className="btn-login" href="/">Log In</Button>
+                            </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
+            </>
         )
     }
 }
---
+++ my-test/src/Pages/Home.js
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+import { Container } from "react-bootstrap";
+import './Home.css';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <Container>
+                <div>
+                    <div>
+                        <div className="baner">
+                            <h1>Time is money,<br/> don’t waste it</h1>
+
+                        </div>
+                    </div>
+                    <div></div>
+                </div>
+            </Container>
+        )
+    }
+}
